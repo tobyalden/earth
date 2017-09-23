30910c02735b2d6e8a83ca455865a8e0cf016356
Toby Alden <toby.alden@gmail.com>
2017-09-23 16:04:55 -0700

    Normalize tabs

--- source/Main.hx
+++ source/Main.hx
@@ -6,9 +6,9 @@ import openfl.display.Sprite;
 class Main extends Sprite
 {
     public function new()
-	{
-	    super();
-	    addChild(new FlxGame(0, 0, PlayState, 1, 60, 60, true));
+    {
+        super();
+        addChild(new FlxGame(0, 0, PlayState, 1, 60, 60, true));
         FlxG.mouse.visible = false;
     }
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,18 +7,18 @@ class PlayState extends FlxState
 {
     private var map:FlxTilemap;
 
-	override public function create():Void
-	{
-		super.create();
+    override public function create():Void
+    {
+        super.create();
         var mapPath = 'assets/data/maps/0.png';
         var tilesetPath = 'assets/images/tiles.png';
         map = new FlxTilemap();
         map.loadMapFromGraphic(mapPath, false, 1, tilesetPath, 16, 16, AUTO);
         add(map);
-	}
+    }
 
-	override public function update(elapsed:Float):Void
-	{
-		super.update(elapsed);
-	}
+    override public function update(elapsed:Float):Void
+    {
+        super.update(elapsed);
+    }
 }
