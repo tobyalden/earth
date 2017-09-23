ec9ec6999c808f205b016e1f7f45b3e3241184f0
Toby Alden <toby.alden@gmail.com>
2017-09-23 15:42:55 -0700

    Initial commit

---
+++ source/Main.hx
@@ -0,0 +1,14 @@
+package;
+
+import flixel.*;
+import openfl.display.Sprite;
+
+class Main extends Sprite
+{
+    public function new()
+	{
+	    super();
+	    addChild(new FlxGame(0, 0, PlayState, 1, 60, 60, true));
+        FlxG.mouse.visible = false;
+    }
+}
