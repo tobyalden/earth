6dec43a607f95f48a4034eacfe4548fcb610a355
Toby Alden <toby.alden@gmail.com>
2017-09-23 16:02:56 -0700

    Load base map

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,20 @@
 package;
 
-import flixel.FlxState;
+import flixel.*;
+import flixel.tile.*;
 
 class PlayState extends FlxState
 {
+    private var map:FlxTilemap;
+
 	override public function create():Void
 	{
 		super.create();
+        var mapPath = 'assets/data/maps/0.png';
+        var tilesetPath = 'assets/images/tiles.png';
+        map = new FlxTilemap();
+        map.loadMapFromGraphic(mapPath, false, 1, tilesetPath, 16, 16, AUTO);
+        add(map);
 	}
 
 	override public function update(elapsed:Float):Void
