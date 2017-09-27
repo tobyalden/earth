f6ec9660a4504edab6c3fba7c6024a31fd740ec2
Toby Alden <toby.alden@gmail.com>
2017-09-27 09:47:11 -0700

    Refactor level generation into level class

--- source/Level.hx
+++ source/Level.hx
@@ -5,9 +5,14 @@ import flixel.tile.*;
 
 class Level extends FlxTilemap
 {
+    public static inline var MAX_SEGMENT_INDEX = 10;
+
+    public var segments:Map<String, Segment>;
+
     public function new(path:String)
     {
         super();
+        segments = new Map<String, Segment>();
         loadMapFromGraphic(path, false, 1, 'assets/images/tiles.png');
     }
 
@@ -24,4 +29,44 @@ class Level extends FlxTilemap
         }
         return super.getTile(tileX, tileY);
     }
+
+    public function generate() {
+        for (x in 0...widthInTiles) {
+            for (y in 0...heightInTiles) {
+                if(hasSegment(x, y)) {
+                    var rand = FlxG.random.int(0, MAX_SEGMENT_INDEX);
+                    var segmentPath = 'assets/data/segments/' + rand + '.png';
+                    var segment = new Segment(segmentPath);
+                    segment.x = x * segment.width;
+                    segment.y = y * segment.height;
+                    sealSegment(x, y, segment);
+                    segments.set([x, y].toString(), segment);
+                }
+            }
+        }
+    }
+
+    private function sealSegment(x:Int, y:Int, segment:Segment) {
+        if(!hasSegment(x - 1, y)) {
+            for (y in 0...segment.heightInTiles) {
+                segment.setTile(0, y, 1);
+            }
+        }
+        if(!hasSegment(x + 1, y)) {
+            for (y in 0...segment.heightInTiles) {
+                segment.setTile(segment.widthInTiles - 1, y, 1);
+            }
+        }
+        if(!hasSegment(x, y - 1)) {
+            for (x in 0...segment.widthInTiles) {
+                segment.setTile(x, 0, 1);
+            }
+        }
+        if(!hasSegment(x, y + 1)) {
+            for (x in 0...segment.widthInTiles) {
+                segment.setTile(x, segment.heightInTiles - 1, 1);
+            }
+        }
+    }
+
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,7 +5,6 @@ import flixel.*;
 class PlayState extends FlxState
 {
     public static inline var MAX_LEVEL_INDEX = 10;
-    public static inline var MAX_SEGMENT_INDEX = 10;
 
     private var level:Level;
     private var currentSegment:Segment;
@@ -19,25 +18,17 @@ class PlayState extends FlxState
         var rand = FlxG.random.int(0, MAX_LEVEL_INDEX);
         var levelPath = 'assets/data/levels/' + rand + '.png';
         level = new Level(levelPath);
-        for (x in 0...level.widthInTiles) {
-            for (y in 0...level.heightInTiles) {
-                    if(level.getTile(x, y) == 1) {
-                    rand = FlxG.random.int(0, MAX_SEGMENT_INDEX);
-                    var segmentPath = 'assets/data/segments/' + rand + '.png';
-                    var segment = new Segment(segmentPath);
-                    segment.x = x * segment.width;
-                    segment.y = y * segment.height;
-                    sealSegment(x, y, segment);
-                    currentSegment = segment; // temp
-                    add(segment);
-                }
-            }
-        }
+        level.generate();
         FlxG.worldBounds.set(
             0, 0,
             level.widthInTiles * FlxG.width,
             level.heightInTiles * FlxG.height
         );
+
+        for(segment in level.segments) {
+            add(segment);
+            currentSegment = segment; // temp
+        }
 
         // Add player & option
         player = new Player(
@@ -46,29 +37,6 @@ class PlayState extends FlxState
         option = new Option(player);
         add(player);
         add(option);
-    }
-
-    private function sealSegment(x:Int, y:Int, segment:Segment) {
-        if(!level.hasSegment(x - 1, y)) {
-            for (y in 0...segment.heightInTiles) {
-                segment.setTile(0, y, 1);
-            }
-        }
-        if(!level.hasSegment(x + 1, y)) {
-            for (y in 0...segment.heightInTiles) {
-                segment.setTile(segment.widthInTiles - 1, y, 1);
-            }
-        }
-        if(!level.hasSegment(x, y - 1)) {
-            for (x in 0...segment.widthInTiles) {
-                segment.setTile(x, 0, 1);
-            }
-        }
-        if(!level.hasSegment(x, y + 1)) {
-            for (x in 0...segment.widthInTiles) {
-                segment.setTile(x, segment.heightInTiles - 1, 1);
-            }
-        }
     }
 
     override public function update(elapsed:Float):Void
